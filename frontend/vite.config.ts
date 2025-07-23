ca66792d64b6f3b7566122b4911ea626d6b3a10f
wx-yz <57770159+wx-yz@users.noreply.github.com>
2025-07-22 17:36:38 -0700

    allowed host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 8080,
-    host: true
+    host: true,
+    allowedHosts: ['backend-1913708723:5001', 'frontend-2637790133.dp-development-menugenapp-161-2326912226']
   },
   preview: {
     port: 8080,
