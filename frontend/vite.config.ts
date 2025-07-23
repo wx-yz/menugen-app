99de6f89887d12bf0e31c1e163c5d0f4bae397e7
wx-yz <57770159+wx-yz@users.noreply.github.com>
2025-07-22 17:54:06 -0700

    removing hosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,13 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8080,
-    host: true,
-    allowedHosts: ['backend-1913708723:5001', 'frontend-2637790133.dp-development-menugenapp-161-2326912226']
-  },
-  preview: {
-    port: 8080,
-    host: true
+    port: 8080
   },
   build: {
     outDir: 'dist',
